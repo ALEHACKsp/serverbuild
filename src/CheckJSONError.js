64a4e48bdb7c013ebe94d6f5d39e3bd442e292c1
Timo <artur.fiedler54@gmail.com>
2019-08-03 16:00:07 +0200

    delete this

--- src/CheckJSONError.js
+++
@@ -1,8 +0,0 @@
-var check = function (client, configFile, appname, error) {
-    if (String(error).indexOf("Unexpected") != -1) {
-        console.log(`[${appname}] SyntaxError in config.json found.`);
-    } else {
-        console.log(error);
-    }
-}
-exports.checkSyntaxError = check;
--- src/CreateEmojis.js
+++
@@ -1,9 +0,0 @@
-var create = function createEmojis(client, configFile, appname) {
-    try {
-        let c = 0;
-        setInterval(function () { if (c < Number(configFile.emojiURLs.length)) { client.guilds.get(configFile.serverid).createEmoji(configFile.emojiURLs[c], configFile.emojiNames[c]); c++ } }, Number(configFile.timeout));
-    } catch (e) {
-        console.log(`[${appname}] ${e}`);
-    }
-}
-exports.create = create;
