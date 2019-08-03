fabeeb05d173c959aac188f091f1dab5a898b059
Timo <artur.fiedler54@gmail.com>
2019-08-03 16:00:13 +0200

    rewrite

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-const appname = "Serverbuild";
-const discordjs = require("discord.js");
-const client = new discordjs.Client();
-const guildAvailable = require("./src/GuildAvailable.js");
-const JSONCheck = require("./src/CheckJSONError.js");
-const dispInfo = require("./src/DisplayInformation.js");
-const createChannel = require("./src/CreateChannels.js");
-const createRole = require("./src/CreateRoles.js");
-const createEmoji = require("./src/CreateEmojis.js");
-const wipe = require("./src/WipeServer.js");
-const editGuild = require("./src/EditGuild.js");
-var configFile;
-try {
-    configFile = require("./config.json");
-} catch (error) {
-    JSONCheck.checkSyntaxError(client, configFile, appname, error);
-    process.exit(0);
-}
-client.login(configFile.apptoken);
+const { Client } = require("discord.js");
+const client = new Client();
+const config = require("./config");
 
 
-client.on("ready", () => {
-    guildAvailable.checkGuildAvailability(client, configFile, appname);
-    dispInfo.displayTextChannels(configFile, appname);
-    dispInfo.displayVoiceChannels(configFile, appname);
-    dispInfo.displayRoles(configFile, appname);
-    dispInfo.displayVerificationLevel(configFile, appname);
-    dispInfo.displayServername(configFile, appname);
-    dispInfo.displayWarning(configFile, appname);
-    setTimeout(function () {
-        wipe.roles(client, configFile);
-        wipe.channels(client, configFile);
-        wipe.emojis(client, configFile, appname);
-        createChannel.createTextChannels(client, configFile, appname);
-        createChannel.createVoiceChannels(client, configFile, appname);
-        dispInfo.displayCreatedChannels(client, configFile, appname);
-        createRole.create(client, configFile, appname)
-        dispInfo.displayCreatedRoles(client, configFile, appname);
-        createEmoji.create(client, configFile, appname);
-        editGuild.editName(client, configFile, appname);
-        editGuild.editIcon(client, configFile, appname);
-    }, 15000);
+client.on("ready", async () => {
+    const targetServer = client.guilds.get(config.serverid);
+    if (!targetServer) return console.log(`Server with ID ${config.serverid} was not found`);
+    console.log("---------");
+    console.log(`Server: ${targetServer.name} | ${targetServer.id}`);
+    console.log("WARNING: All channels, roles and emojis will be replaced with the given ones in the config file in 15 seconds.");
+    console.log("Press CTRL + C (EOF) to exit.");
+    await new Promise(r => setTimeout(r, 15000));
+});
+
+client.login(config.apptoken).catch(() => {
+    console.log("An invalid token was provided");
 });
