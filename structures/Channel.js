fc7b893ba7b41474dfb08276fd1bf4d49ecadac2
Timo <artur.fiedler54@gmail.com>
2019-11-23 17:31:38 +0000

    add Channel structure

---
+++ structures/Channel.js
@@ -0,0 +1,10 @@
+module.exports = class Channel {
+    constructor(ChannelData) {
+        this.type = ChannelData.type;
+        this.position = ChannelData.position;
+        this.permissionOverwrites = ChannelData.permissionOverwrites;
+        this.topic = ChannelData.topic;
+        this.name = ChannelData.name;
+        this.rateLimitPerUser = ChannelData.rateLimitPerUser;
+    }
+}
